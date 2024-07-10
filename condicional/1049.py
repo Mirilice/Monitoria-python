261d0a4e9d6062b018a257072051c7f8019822df
Mirilice <mariaalicepco10@gmail.com>
2024-07-10 15:36:05 -0300

    Informações

---
+++ condicional/1049.py
@@ -0,0 +1,6 @@
+# Entrada
+# A entrada contém 3 palavras, uma em cada linha, necessárias para identificar o
+# animal segundo a figura acima, com todas as letras minúsculas.
+
+# Saída
+# Imprima o nome do animal correspondente à entrada fornecida.
