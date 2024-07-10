db5ed7fa42d77100f21afc183c36b584acf15b74
Mirilice <mariaalicepco10@gmail.com>
2024-07-10 17:05:25 -0300

    Atualização

---
+++ condicional/1048.py
@@ -0,0 +1,9 @@
+# Leia o salário do funcionário e calcule e mostre o novo salário, 
+# bem como o valor de reajuste ganho e o índice reajustado, em percentual.
+
+# Entrada
+# A entrada contém apenas um valor de ponto flutuante, com duas casas decimais.
+
+# Saída
+# Imprima 3 linhas na saída: o novo salário, o valor ganho de reajuste 
+# (ambos devem ser apresentados com 2 casas decimais) e o percentual de reajuste ganho, conforme exemplo abaixo.
